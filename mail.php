705834c00266451abed81c1d91a1429f6059fb98
Bradley Ruiz <bradleyruiz@gmail.com>
2013-06-24 22:14:55 +0000

    fixed mail.php to not allow blank fields

--- mail.php
+++ mail.php
@@ -1,9 +1,12 @@
 <?php
 	//declare our assets 
+	
 	$name = stripcslashes($_POST['name']);
 	$emailAddr = stripcslashes($_POST['email']);
 	$comment = stripcslashes($_POST['message']);
 	$subject = stripcslashes($_POST['subject']);	
+        if (($name !="") and ($emailAddr !="") and ($comment !=""))
+	{
 	$contactMessage =  
 		"Message:
 		$comment 
@@ -11,10 +14,13 @@
 		Name: $name
 		E-mail: $emailAddr
 
-		Sending IP:$_SERVER[REMOTE_ADDR]
-		Sending Script: $_SERVER[HTTP_HOST]$_SERVER[PHP_SELF]";
-		
+                Sending IP:$_SERVER[REMOTE_ADDR]
+                Sending Script: $_SERVER[HTTP_HOST]$_SERVER[PHP_SELF]";
 		//send the email 
 		mail('admin@metalignus.com', $subject, $contactMessage, "From: $emailAddr");
 		echo('success'); //return success callback
+	}
+	
+	else {
+	echo ('Please fill all the information'); }
 ?>
