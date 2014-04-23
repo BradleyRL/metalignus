20333701346d8d5f712059055c571d8ce9cfa755
Bradley Ruiz <bradleyruiz@gmail.com>
2014-04-23 15:03:51 -0600

    fixed config for new url

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://metalignus.com';
+$config['base_url']	= 'http://metalignus-10.appspot.com';
 
 /*
 |--------------------------------------------------------------------------
