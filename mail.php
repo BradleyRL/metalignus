0cd29d98f1836be30e63d1bd90b5151dd15340c0
Bradley Ruiz <bradleyruiz@gmail.com>
2014-04-24 09:52:51 -0600

    fix mail stuff

--- mail.php
+++ mail.php
@@ -1,4 +1,6 @@
 <?php
+	require_once 'google/appengine/api/mail/Message.php';
+	use php\sdk\google\appengine\api\mail\Message;
 	//declare our assets 
 	
 	$name = stripcslashes($_POST['name']);
@@ -17,8 +19,22 @@
                 Sending IP:$_SERVER[REMOTE_ADDR]
                 Sending Script: $_SERVER[HTTP_HOST]$_SERVER[PHP_SELF]";
 		//send the email 
-		mail('admin@metalignus.com', $subject, $contactMessage, "From: $emailAddr");
+		//mail('admin@metalignus.com', $subject, $contactMessage, "From: $emailAddr");
+		$mail_options = [
+			"sender" => $emailAddr,
+			"to" => "admin@metalignus.com",
+			"subject" => $subject,
+			"textBody" => $contactMessage
+		];
+
+	try {
+		$message = new Message($mail_options);
+		$message->send();
 		echo('success'); //return success callback
+	} catch (InvalidArgumentException $e) {
+		echo('error');
+	}
+		
 	}
 	
 	else {
