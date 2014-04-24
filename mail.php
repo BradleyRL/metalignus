a3b651762f3fd2f07ac27438b302ceab3497d7aa
Bradley Ruiz <bradleyruiz@gmail.com>
2014-04-24 10:09:21 -0600

    fix mail stuff

--- mail.php
+++ mail.php
@@ -1,6 +1,6 @@
 <?php
 	require_once 'google/appengine/api/mail/Message.php';
-	use php\sdk\google\appengine\api\mail\Message;
+	//use php\sdk\google\appengine\api\mail\Message;
 	//declare our assets 
 	
 	$name = stripcslashes($_POST['name']);
