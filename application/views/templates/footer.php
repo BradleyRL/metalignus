ff55d97a6b87a753ca1cf94d8b8365e6a192cab9
Bradley Ruiz <bradleyruiz@gmail.com>
2014-04-24 13:52:25 -0600

    updated footer

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,4 +1,4 @@
- <div id="footer"><p id="footer-copyright">&copy; 2013 Metalignus.com</p><p id="footer-menu"><a href="home">Home</a> | <a 
+ <div id="footer"><p id="footer-copyright">&copy; 2013-2014 Metalignus.com</p><p id="footer-menu"><a href="home">Home</a> | <a 
 href="portfolio">Applications</a></p></div>
   </div><!-- /container -->
  </body>
