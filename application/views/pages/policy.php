1c01740b37259c788a19545ffcfea6e495623b96
unknown <br36889@LACGT01H01194.lac.nsroot.net>
2017-02-22 11:07:40 -0600

    private policy

--- application/views/pages/policy.php
+++ application/views/pages/policy.php
@@ -6,16 +6,17 @@
   <p>
   <strong>Application Name</strong>: Devil Kernel Manager<br />
   </p>
-  <p>WEdo not collect or store any personal information about users of Devil kernel Manager.  The identities of the users of this app is not known. And if any personal information about a user is ever received, it will not be disclosed or shared with a third party.
+  <p>We do not collect or store any personal information about users of Devil kernel Manager.  The identities of the users of this app is not known. And if any personal information about a user is ever received, it will not be disclosed or shared with a third party.
+  <br /><br />
   The permission to read your contacts is only used to configure and use the Back Light Notification (BLN) settings of the kernel.
   You can easily disable this on the settings, its used to provide app functionality only and we do not receive any information from these permissions.
-  
-  == What is the BLN? ==
-The BLN mod turns the touchkey backlights into a notification led.
+  <br /><br /><br />
+  == What is the BLN? ==<br />
+The BLN mod turns the touchkey backlights into a notification led.<br />
 It enables any android application to use the touchkey backlights of your phone as a notification led.
-
+<br /><br />
 This app do not track behavior or usage statistics with analytics frameworks such as Google Analytics or Crashlytics.  Some details about app installation and usage is provided by the Google Play Developer Console.  This does not contain any personally identifying information and is not shared with third parties.
-
+<br /><br /><br />
 If you have any questions about this Privacy Policy, please contact: support@metalignus.com
 <br />
 <br />
