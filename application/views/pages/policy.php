dbb5452e466e85fd8f128c21bbd02dab765984f6
unknown <br36889@LACGT01H01194.lac.nsroot.net>
2017-02-22 11:15:45 -0600

    private policy

--- application/views/pages/policy.php
+++ application/views/pages/policy.php
@@ -10,13 +10,13 @@
   <br /><br />
   The permission to read your contacts is only used to configure and use the Back Light Notification (BLN) settings of the kernel.
   You can easily disable this on the settings, its used to provide app functionality only and we do not receive any information from these permissions.
-  <br /><br /><br />
+  <br /><br />
   == What is the BLN? ==<br />
-The BLN mod turns the touchkey backlights into a notification led.<br />
+The BLN mod turns the touchkey backlights into a notification led.
 It enables any android application to use the touchkey backlights of your phone as a notification led.
 <br /><br />
 This app do not track behavior or usage statistics with analytics frameworks such as Google Analytics or Crashlytics.  Some details about app installation and usage is provided by the Google Play Developer Console.  This does not contain any personally identifying information and is not shared with third parties.
-<br /><br /><br />
+<br /><br />
 If you have any questions about this Privacy Policy, please contact: support@metalignus.com
 <br />
 <br />
