e505f988a3de93b13c1c7f07fdf3164eb861647c
unknown <br36889@LACGT01H01194.lac.nsroot.net>
2017-02-22 10:31:35 -0600

    new footer

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,5 +1,5 @@
- <div id="footer"><p id="footer-copyright">&copy; 2013-2014 Metalignus.com</p><p id="footer-menu"><a href="home">Home</a> | <a 
-href="portfolio">Applications</a></p></div>
+ <div id="footer"><p id="footer-copyright">&copy; 2013-2017 Metalignus.com  | Contact: support@metalignus.com</p><p id="footer-menu"><a href="home">Home</a> | <a 
+href="portfolio">Applications</a>| <a href="policy">Private Policy</a></p></div>
   </div><!-- /container -->
  </body>
 </html>
